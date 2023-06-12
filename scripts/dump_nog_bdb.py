ea62ba1784919afb81c38402b93938745a68cfb5
jsjiang <jing.jiang@ucop.edu>
2023-06-12 16:40:32 -0700

    save output json in subdir follow minter path name convention

--- scripts/dump_nog_bdb.py
+++ scripts/dump_nog_bdb.py
@@ -15,7 +15,7 @@ def find_files(root, pattern):
             matches.append(os.path.join(path, filename))
     return matches
 
-def dump_nog_file(db_path, output_file):
+def dump_nog_file(db_path, output_dir):
 
     db = bsddb3.btopen(db_path, 'r')
     items = db.items()
@@ -24,46 +24,47 @@ def dump_nog_file(db_path, output_file):
     for key, value in items:
         data[key.decode()] = value.decode()
     
-    with open(output_file, 'w') as writer:
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir, ignore_errors=True)
+    else:
+        os.makedirs(output_dir)
+
+    with open(os.path.join(output_dir, "nog_bdb.json"), 'w') as writer:
         json.dump(data, writer)
 
     db.close()
 
-def create_output_filename(file_path):
-    """Create output filename based on file path.
+def find_minter_path(file_path):
+    """Find minter path based on the dbd file path.
     
-       Find the "shoulder_and_id" part from the file_path, for example /apps/ezid/var/minters/minters/f9999/c5/nog.bdb.
-       The "shoulder_and_id" is "/f9999/c5/" in this case.
-       Replace "/" with "_" in the matched string.
-       Return matched string as filename, "_f9999_c5" in this case.
-
+       For example when the file path  /apps/ezid/var/minters/minters/f9999/c5/nog.bdb, 
+       the minter path is "/f9999/c5/".
     """
-    filename = None
+    minterpath = None
     start_substring = "minters"
     end_substring = "nog.bdb"
     pattern = r"%s(.*?)%s" % (re.escape(start_substring), re.escape(end_substring))
     match = re.search(pattern, file_path)
     if match:
-        filename = match.group(1)
-        filename = filename.replace("/", "_")
-        filename = f"{filename}nogdb.out"
+        minterpath = match.group(1)
     
-    return filename 
+    return minterpath
 
 
 def main():
-    if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} minters_path")
+    if len(sys.argv) != 3:
+        print(f"Usage: {sys.argv[0]} minters_path output_dir_name")
         exit()
 
     minters_path = sys.argv[1]
+    output_dir = sys.argv[2]
     file_pattern = 'nog.bdb' 
     ct_1 = datetime.datetime.now()
     print(f"start BDB dumping: {ct_1}")
     print(f"for Minters path: {minters_path}")
 
     ct = ct_1.strftime('%Y%m%d_%H%M%S')
-    output_dir = f"./nog_db_output/{ct}"
+    output_dir = f"{output_dir}/{ct}"
     print(f"save ouput files in: {output_dir}")
     if os.path.exists(output_dir):
         shutil.rmtree(output_dir, ignore_errors=True)
@@ -74,11 +75,11 @@ def main():
     print(f"number of nog files to dump: {len(nog_files)}")
     for nog_db_file in nog_files:
         print(f"dump db file: {nog_db_file}")
-        output_filename = create_output_filename(nog_db_file)
-        if output_filename:
-            output_filename =  f"{output_dir}/{output_filename}"
-            print(f"output filename: {output_filename}")
-            dump_nog_file(nog_db_file, output_filename)
+        minter_path = find_minter_path(nog_db_file)
+        if minter_path:
+            output_file_dir = f"{output_dir}{minter_path}"
+            print(f"output file dir: {output_file_dir}")
+            dump_nog_file(nog_db_file, output_file_dir)
         else:
             print(f"error: failed to create output filename based on nog db path {nog_db_file}")
     
