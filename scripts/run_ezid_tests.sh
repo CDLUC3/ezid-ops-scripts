da0f5ac8dad8913227c3e5d25bf3d27e7ce78dc7
jsjiang <jing.jiang@ucop.edu>
2025-06-20 16:40:38 -0700

    test debug option

--- scripts/run_ezid_tests.sh
+++ scripts/run_ezid_tests.sh
@@ -13,7 +13,7 @@ set -e  # Exit on first failure
 
 if [ $# -ne 4 ] && [ $# -ne 5 ]; then
   echo "Error: You must provide either 4 or 5 arguments."
-  echo "Usage: $0 <environment> <username> <password> <email> <debug_flag>"
+  echo "Usage: $0 <environment dev|stg|prd> <username> <password> <email> <optional_debug_flag>"
   exit 1
 fi
 
@@ -32,15 +32,25 @@ echo "Starting the EZID test suite..."
 echo "Running functional tests..."
 python verify_ezid_status.py -e $ENV -u $USER -p $PASSWORD -n $EMAIL
 
-# Start docker standalone Chrome container
+# Start standalone Chrome container
 if [ "$5" == "debug" ]; then
+  echo "Removing Selenium container in case it is up and running"
+  docker rm -f selenium
   echo "Starting Selenium standalone Chrome container..."
   docker run -d -p 4444:4444 --name selenium seleniarm/standalone-chromium:latest
+  echo "Waiting for Selenium to be ready..."
+  until curl -sf http://localhost:4444/wd/hub/status | grep -q '"ready": true'; do
+    sleep 2
+    echo "Waiting..."
+  done
 else
   echo "Selenium container should have been started. Otherwise, UI tests will fail."
 fi
 
 echo "Running UI tests..."
 python ezid_ui_tests_docker.py -e $ENV -u $USER -p $PASSWORD -n $EMAIL
+
+echo "Removing Selenium container"
+docker rm -f selenium
 
 echo "EZID tests completed successfully!"
